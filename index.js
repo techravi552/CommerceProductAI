9d36965076c51b3f2952c51b6d9b64b59db84b23
Ravi Kalmodiya <ravikalmodiya552gamil.com>
2025-08-02 20:37:27 +0530

    e comers api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express= require("express")
 const app = express()
 
 app.get("/" , (req , res)=>{
-    res.send(" creat the API")
+    res.send(" creat the E-commerse API..")
 })
 
 app.listen(3000 , ()=>{
